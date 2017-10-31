51c5a1cfa60f8cbc8bdbc3700cd29f276ac79c12
sict <jhpark870708@daum.net>
2017-10-31 11:36:13 +0900

    My first commit

---
+++ app/src/main/java/com/example/sict/happierproject/LocationBaseActivity.java
@@ -0,0 +1,13 @@
+package com.example.sict.happierproject;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class LocationBaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_location_base);
+    }
+}
